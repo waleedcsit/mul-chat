bfce7f7e8b4e9869da5d38b96e3fa588aaa2cc32
Waleed Younas <waleedislive@gmail.com>
2021-06-07 15:09:06 +0500

    new files

---
+++ start_services.sh
@@ -0,0 +1,13 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red31\green31\blue31;\red239\green239\blue239;}
+{\*\expandedcolortbl;;\cssrgb\c16078\c16078\c16078;\cssrgb\c94902\c94902\c94902;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs32 \cf2 \cb3 \expnd0\expndtw0\kerning0
+cd app/\cb1 \
+\cb3 # Start rasa server with nlu model\cb1 \
+\cb3 rasa run --model models --enable-api --cors "*" --debug \\\cb1 \
+\cb3          -p $PORT}
